e226b20772879185af74e579ff6a8c82b7833539
Gabriel Graca <127799792+gracagabriel@users.noreply.github.com>
2024-04-06 13:20:18 -0300

    Initial commit

---
+++ src/components/icon-button.tsx
@@ -0,0 +1,18 @@
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface IconButtonProps extends ComponentProps<'button'> {}
+
+export function IconButton(props: IconButtonProps){
+    return (
+        <button
+            {...props} 
+            //className='bg-black/20 border border-white/10 rounded-md p-1.5'>
+            //{props.children}
+            className={twMerge(
+                'bg-black/20 border border-white/10 rounded-md p-1.5',
+                props.disabled ? 'opacity-50' : null,
+            )}
+        />
+    )
+}
